608e0890f51506704efdaa13c9f99e140781db32
Hema Chandra Gupta Nalluri <nallurichandu546@gmail.com>
2022-08-13 08:02:28 +0530

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with HC
+          Learn React with HC!
         </a>
       </header>
     </div>
