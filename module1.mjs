02d458c0eeda33f164387057dfb74a44a80e916c
Hema Chandra Gupta Nalluri <nallurichandu546@gmail.com>
2022-08-19 12:12:37 +0530

    Added props and default props to the newly created navbar component.

---
+++ module1.mjs
@@ -0,0 +1,7 @@
+import dza, {b,c,d,e} from './module2.mjs'
+
+console.log(dza);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Hema Chandra";
+const b = "Mounica";
+const c = "Alekhya";
+const d = "Ramadevi";
+const e = "Kumar";
+
+
+export default a;
+export {b,c,d,e};
