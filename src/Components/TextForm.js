fa55f3a32a55e56b8ae3d8b88bcc1385f6593718
Hema Chandra Gupta Nalluri <nallurichandu546@gmail.com>
2022-08-19 13:50:21 +0530

    Added button to convert text to lower case and also added logic to calculate number of words, characters and time to read the text.

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
-  const [text, setText] = useState('Enter text here');
+  const [text, setText] = useState('');
   // text = "new state text" //wrong way to change the state of the text, we have change the state through function only.
   // setText("Enter new text here"); // correct way to change the state
     const handleOnChange = (event) => {
@@ -14,13 +14,29 @@ export default function TextForm(props) {
         // console.log("Uppercase button is clicked!");
         setText(text.toUpperCase());
     }
+
+    const btnLowercase = () => {
+        // console.log("Uppercase button is clicked!");
+        setText(text.toLocaleLowerCase());
+    }
+
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control"  value = {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+    <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control"  value = {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={btnUppercase}>Convert To Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={btnLowercase}>Convert To Lowercase</button>
         </div>
-        <button className="btn btn-primary" onClick={btnUppercase}>Convert To Uppercase</button>
-    </div>
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{parseFloat(0.008 * (text.split(" ").length)).toFixed(2)} minutes to read the text.</p>
+            <h3>Preview</h3>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
