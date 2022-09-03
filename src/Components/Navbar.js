556b196d237c430472ec3b0480ac1649fa09aebc
Hema Chandra Gupta Nalluri <nallurichandu546@gmail.com>
2022-09-03 16:44:02 +0530

    Removed unwanted code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,10 +19,6 @@ export default function NavBar(props) {
                     <Link className="nav-link"  to="/textutils/about">{props.aboutText}</Link>
                 </li>
             </ul>
-        {/* <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-primary" type="submit">Search</button>
-        </form> */}
         <div className={`form-check form-switch text-${props.mode === 'dark'? 'light' : 'dark'}`}>
           <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
           <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
